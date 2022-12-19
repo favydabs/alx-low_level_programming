988409fd16baf4c2244aec80e7213e8c7c43c6da
favydabs <favourdabit@gmail.com>
2022-12-19 08:00:13 -0800

    reset 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* reset_to_98.c and update the value its point to 98
+* @n: parameter to be provided
+* Return: Always 0 (success)
+*/
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
